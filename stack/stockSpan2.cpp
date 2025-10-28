10ac1e779b38ab9be4d267bc89912795c4cf2fea
[Wasim Akhtar Khan] <wasimakhtarkhan2003@gmail.com>
2025-10-29 03:13:55 +0530

    Stock Span using Pair Stack

---
+++ stack/stockSpan2.cpp
@@ -0,0 +1,21 @@
+class StockSpanner {
+public:
+    stack<pair<int, int>> st; // {price,span}
+    StockSpanner() {}
+
+    int next(int price) {
+        int span = 1;
+        while (!st.empty() && price >= st.top().first) {
+            span = span + st.top().second; // accumulate the span
+            st.pop();
+        }
+        st.push({price, span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
