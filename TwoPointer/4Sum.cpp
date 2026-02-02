8dac6fd6cb6e9a7c093a2db3a969cef7746b3f04
Wasim Akhtar <wasimakhtarkhan2003@gmail.com>
2026-02-03 02:15:30 +0530

    Added : 3Sum and 4Sum

---
+++ TwoPointer/4Sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        int n=nums.size();
+        if(n<4) return ans;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int left=j+1;
+                int right=n-1;
+                while(left < right){
+                    long long sum=(long long)nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target){
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        left++;
+                        right--;
+                        while(left < right && nums[left]==nums[left-1]) left++;
+                        while(left < right && nums[right]==nums[right+1]) right--;
+                    }else if(sum < target){
+                        left++;
+                    }else if(sum > target){
+                        right--;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
